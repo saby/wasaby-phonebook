93b75a443b808a4aca79875df2f3dd71d33d91a3
oa.kashin <oa.kashin@tensor.ru>
2020-06-26 16:58:54 +0300

    3. Добавлено редактирование данных.

--- PhoneBook-demo/PhoneBook/PhoneBook.ts
+++ PhoneBook-demo/PhoneBook/PhoneBook.ts
@@ -2,6 +2,8 @@ import { Control, IControlOptions, TemplateFunction } from 'UI/Base';
 import * as template from 'wml!PhoneBook-demo/PhoneBook/PhoneBook';
 import { Memory } from 'Types/source';
 import { View, IColumn } from 'Controls/grid';
+import * as columnTemplate from 'wml!PhoneBook-demo/PhoneBook/columnTemplate';
+import * as phoneColumnTemplate from 'wml!PhoneBook-demo/PhoneBook/phoneColumnTemplate';
 
 interface IPhoneBookRecord {
     Address: string;
@@ -19,13 +21,16 @@ export default class PhoneBook extends Control<IPhoneBookOptions> {
     protected _source: Memory;
     protected _columns: IColumn[] = [{
         displayProperty: 'FIO',
+        template: columnTemplate,
         width: '230px'
     }, {
         displayProperty: 'Address',
+        template: columnTemplate,
         width: '400px'
     }, {
         displayProperty: 'Phone',
-        width: '170px'
+        template: phoneColumnTemplate,
+        width: '270px'
     }];
 
     protected _children: {
