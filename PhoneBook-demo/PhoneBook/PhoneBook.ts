636df87375dfa209e96cec4a126d1be09d40491d
oa.kashin <oa.kashin@tensor.ru>
2020-06-26 16:48:38 +0300

    2. Создаем в PhoneBook список - только <Controls.treeGrid:View с колонками. Никакие обработчики дополнительные не назначаем. Делаем только отображение данных, для чего подключаем this._source = new Memory в ts файле. ColumnTemplate не создаем. Настраиваем через displayProperty на колонках.

--- PhoneBook-demo/PhoneBook/PhoneBook.ts
+++ PhoneBook-demo/PhoneBook/PhoneBook.ts
@@ -1,6 +1,47 @@
 import { Control, IControlOptions, TemplateFunction } from 'UI/Base';
 import * as template from 'wml!PhoneBook-demo/PhoneBook/PhoneBook';
+import { Memory } from 'Types/source';
+import { View, IColumn } from 'Controls/grid';
 
-export default class PhoneBook extends Control<IControlOptions> {
+interface IPhoneBookRecord {
+    Address: string;
+    FIO: string;
+    key: number;
+    Phone: string;
+}
+
+interface IPhoneBookOptions extends IControlOptions {
+    data: IPhoneBookRecord[];
+}
+
+export default class PhoneBook extends Control<IPhoneBookOptions> {
     protected _template: TemplateFunction = template;
+    protected _source: Memory;
+    protected _columns: IColumn[] = [{
+        displayProperty: 'FIO',
+        width: '230px'
+    }, {
+        displayProperty: 'Address',
+        width: '400px'
+    }, {
+        displayProperty: 'Phone',
+        width: '170px'
+    }];
+
+    protected _children: {
+        gridView: View & Control;
+    };
+
+    protected _beforeMount(options?: IPhoneBookOptions): void {
+        this._source = new Memory({
+            keyProperty: 'key',
+            data: options.data
+        });
+    }
+
+    static getDefaultOptions(): IPhoneBookOptions {
+        return {
+            data: []
+        };
+    }
 }
